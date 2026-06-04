1726f35d1c78332476d25396e8c46c37f79266c6
Nathaniel Parah <nathanielparah579@gmail.com>
2026-06-04 12:29:28 +0100

    removed the intances from the main modeule and added a success message
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,3 @@
-use rust_practice::practice;
 fn main() {
-    let chats: Vec<String> = vec!["1:nathaniel".to_string(), "2:noel".to_string(), "3:nelson".to_string(), "4:nicolas".to_string()];
-    practice::log_parser(chats);
-    println!("Hello, world!");
+    println!("Successful");
 }
