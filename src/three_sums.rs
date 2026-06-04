7375e573c423d10403850b04bc79ea5dc9a242e5
Nathaniel Parah <nathanielparah579@gmail.com>
2026-06-04 19:09:59 +0100

    performed sips on the right values and left values, moved left moved right, and returned the final result as pairs
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/three_sums.rs
+++ src/three_sums.rs
@@ -1,15 +1,15 @@
-fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
+fn three_sum(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
     nums.sort();
     let mut result = Vec::new();
     let len = nums.len();
 
     for i in 0..len{
         if i > 0 && nums[i] == nums[i - 1] {
-            i += 1;
+            continue; 
         }
 
-        let left = i + 1; //this is the element after the first element.
-        let right = len - 1; // this is the last element on the list.
+        let mut left = i + 1; //this is the element after the first element.
+        let mut right = len - 1; // this is the last element on the list.
 
         while left < right {
             let sum = nums[i] + nums[left] + nums[right];
@@ -21,7 +21,19 @@ fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
             } else {
                 result.push(vec![nums[i], nums[left], nums[right]]);
             }
+        
+            while left < right && nums[left] == nums[left + 1] {
+                left += 1;
+            }
 
+            while left < right && nums[right] == nums[right - 1] {
+                right -= 1;
+            }
+
+            left += 1;
+            right -= 1;
         }
     }
+    result
+
 }
