6579f2e4e6c1e47ef6921fe41eb1d4c8190f7c85
Nathaniel Parah <nathanielparah579@gmail.com>
2026-06-05 19:03:19 +0100

    Added the outer loop which handles i(the current freezed value)
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/triangle_num.rs
+++ src/triangle_num.rs
@@ -1,8 +1,11 @@
 fn triangle_num(mut num: Vec<i32>) -> i32 {
     num.sort();
     let count = 0;
-    
     let length = num.len();
+
+    for i in (0..length).rev() {
+        
+    }
 
     count
 
