bc50692323f001171e9435678e51eaea74d7086e
Nathaniel Parah <nathanielparah579@gmail.com>
2026-06-05 17:56:01 +0100

    added the return type at the end of the function
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/triangle_num.rs
+++ src/triangle_num.rs
@@ -1,7 +1,9 @@
-fn triangle_num(num: Vec<i32>) -> i32 {
-    `num.sort();
+fn triangle_num(mut num: Vec<i32>) -> i32 {
+    num.sort();
     let count = 0;
     
     let length = num.len();
+
+    count
 
 }
