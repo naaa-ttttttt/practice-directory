b4b825f5367e243605939e278c23de3d3f65d9be
Nathaniel Parah <nathanielparah579@gmail.com>
2026-06-05 19:59:17 +0100

    added the else part of the function that will move the left pointer inward
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/triangle_num.rs
+++ src/triangle_num.rs
@@ -1,22 +1,21 @@
 fn triangle_num(mut num: Vec<i32>) -> i32 {
     num.sort();
-    let count = 0;
+    let mut count = 0;
     let length = num.len();
 
     for i in (0..length).rev() {
-        let left = 0;
-        let right = i - 1;
+        let mut left = 0;
+        let mut right = i - 1;
 
         while left < right {
-            if num[left] + num[right] > i {
-                count += (right - left);
-
+            if num[left] + num[right] > i.try_into().unwrap() {
+                count += right - left;
+                right -= 1;
+            } else {
+                left += 1;
             }
-
         }
-        
     }
 
-    count
-
+    count.try_into().unwrap()
 }
