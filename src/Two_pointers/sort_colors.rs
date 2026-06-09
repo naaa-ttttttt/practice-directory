34f8e85326919aeb380d0a55af288d56c8882a8e
Nathaniel Parah <nathanielparah579@gmail.com>
2026-06-09 15:27:40 +0100

    created pointers, i, left and right, and started the loop
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/Two_pointers/sort_colors.rs
+++ src/Two_pointers/sort_colors.rs
@@ -1,5 +1,11 @@
 fn sort_colors(num: &mut Vec<i32>) {
     let length = num.len();
+    let mut left = 0;
+    let mut right = length - 1;
+    let mut i = 0;
 
+    while i <= right {
+
+    }
     
 }
