c0b802dc25c0b73d276424afc0debf09eb6a87ba
Nathaniel Parah <nathanielparah579@gmail.com>
2026-06-11 04:38:26 +0100

    feat: add right_max and left_max
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/two_pointers/trap_water.rs
+++ src/two_pointers/trap_water.rs
@@ -3,6 +3,10 @@ fn trapping_water(heights: Vec<i32>) -> i32 {
     let length = heights.len();
     let mut left = 0;
     let mut right = length - 1;
+    let mut left_max = 0;
+    let mut right_max = 0;
+
+
 
 
     count 
