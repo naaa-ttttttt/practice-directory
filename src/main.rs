880f7767a466cadc01616aedfc1a7a7989eafa2d
Nathaniel Parah <nathanielparah579@gmail.com>
2026-06-14 22:52:31 +0100

    feat: import the sliding_window and its modules, and call the function for testing
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 //mod two_pointers;
+use crate::sliding_window::max_sum::max_of_subarray;
 mod sliding_window;
 
 fn main() {
+    println!("{:?}", max_of_subarray(vec![2, 1, 5, 1, 3, 2], 3));
+    
     println!("Successful");
 }
