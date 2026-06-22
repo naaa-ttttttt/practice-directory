67c6c514697a318116e49afac6cd9b2bdbdb5b55
Nathaniel Parah <nathanielparah579@gmail.com>
2026-06-22 15:08:41 +0100

    feat: create pointers, trackers, and initialise k as usize
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/sliding_window/max_points.rs
+++ src/sliding_window/max_points.rs
@@ -1,3 +1,10 @@
 pub fn max_points(cards: Vec<i32>, k: i32) -> i32 {
+    let k = k as usize;
+    let left = 0;
+    let right = cards.len() - 1;
+    let window_sum = 0;
+    let max_sum = 0;
+
+
     0
 }
