5a295315473cbb7edaa846baf7ae33318c2f268c
Nathaniel Parah <nathanielparah579@gmail.com>
2026-06-23 06:52:08 +0100

    refactor: make pointers and tracker variables mutable
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/sliding_window/max_points.rs
+++ src/sliding_window/max_points.rs
@@ -1,9 +1,9 @@
 pub fn max_points(cards: Vec<i32>, k: i32) -> i32 {
     let k = k as usize;
-    let left = 0;
-    let right = cards.len() - 1;
+    let mut left = 0;
+    let mut right = cards.len() - 1;
     let mut window_sum = 0;
-    let max_sum = 0;
+    let mut max_sum = 0;
     
     for i in 0..k {
         window_sum += cards[i];
