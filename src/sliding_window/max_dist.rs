ababa1cec1f30d3c4e0459be69b03691c3607e89
Nathaniel Parah <nathanielparah579@gmail.com>
2026-06-24 10:20:20 +0100

    feat: create pointers, trackers, and dictionary to keep track of values and there counts
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/sliding_window/max_dist.rs
+++ src/sliding_window/max_dist.rs
@@ -1,4 +1,11 @@
+use std::collections::HashMap;
+
 pub fn max_sum(nums: Vec<i32>, k: i32) -> i32 {
+    let max_sum = i32::MIN;
+    let left = 0;
+    let freq_map = HashMap::new();
+    let window_sum = 0;
+
     
     0
 }
