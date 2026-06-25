4d257116dfd939467a42b5ed1945b017a9295e60
Nathaniel Parah <nathanielparah579@gmail.com>
2026-06-25 20:08:05 +0100

    feat: create left pointer, hashmap, and tracker to keep track of longest substring
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/sliding_window/variable_window.rs
+++ src/sliding_window/variable_window.rs
@@ -1,3 +1,12 @@
+use std::collections::HashMap;
+
 pub fn longest_substring(words: String) -> i32 {
+    let mut left = 0;
+    let mut freq_map = HashMap::new();
+    let mut longest_substring = 0;
+    
+    for characters in 0..words.len() {
+        
+    }
     0
 }
