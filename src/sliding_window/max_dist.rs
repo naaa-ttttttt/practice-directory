5b06a316d492d712e3a503becc0a53496e49b9fc
Nathaniel Parah <nathanielparah579@gmail.com>
2026-06-25 07:43:35 +0100

    fix: change values to i64, correct window formula, and implement return value
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/sliding_window/max_dist.rs
+++ src/sliding_window/max_dist.rs
@@ -1,22 +1,22 @@
 use std::collections::HashMap;
 
-pub fn max_sum(nums: Vec<i32>, k: i32) -> i32 {
-    let mut max_sum = i32::MIN;
+pub fn max_sum(nums: Vec<i32>, k: i32) -> i64 {
+    let mut max_sum = i64::MIN;
     let mut left = 0;
     let mut freq_map = HashMap::new();
-    let mut window_sum = 0;
+    let mut window_sum = 0i64;
     
     for elements in 0..nums.len() as usize {
-        window_sum += nums[elements];
-        freq_map.entry(nums[elements]).or_insert(0);
+        window_sum += nums[elements] as i64;
+        *freq_map.entry(nums[elements]).or_insert(0) += 1;
 
-        if elements - left == k as usize {
+        if elements - left + 1 == k as usize {
             if freq_map.len() == k as usize {
                 max_sum = max_sum.max(window_sum);
             }
 
-            window_sum -= nums[left] as i32;
-            *freq_map.get_mut(&nums[elements]).unwrap() -= 1;
+            window_sum -= nums[left] as i64;
+            *freq_map.get_mut(&nums[left]).unwrap() -= 1;
             if freq_map[&nums[left]] == 0 {
                 freq_map.remove(&nums[left]);
             }
@@ -24,6 +24,10 @@ pub fn max_sum(nums: Vec<i32>, k: i32) -> i32 {
             left += 1;
         }
     }       
-    
-    0
+
+    if max_sum == i64::MIN {
+       0
+    } else {
+        max_sum
+    }
 }
