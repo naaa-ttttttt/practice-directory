c14f9d76eab4538af6f4cb87e5a9a3a054646d6f
Nathaniel Parah <nathanielparah579@gmail.com>
2026-06-25 23:02:56 +0100

    docs: an algorithm written in form of a comment
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/sliding_window/variable_window.rs
+++ src/sliding_window/variable_window.rs
@@ -8,8 +8,14 @@ pub fn longest_substring(words: String) -> i32 {
     for chars in words.chars() {
         *freq_map.entry(chars).or_insert(0) += 1;
         longest_substring = freq_map.len();
+        
+        // here we are to check the count of the characters, 
+        // and if any characters count is more than one,
+        // we remove the element from the left, and then 
+        // check then move the left pointer forward, 
+        // rerun the code, i guess.
 
-        //if 
+        if  
         
     }
     0
