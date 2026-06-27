9ade39d3bd6e3a0dac2048539a92a5841cb19241
Nathaniel Parah <nathanielparah579@gmail.com>
2026-06-27 23:54:11 +0100

    feat: create loop that moves through elements in the array and push them to the hashmap with there count

--- src/sliding_window/char_replacement.rs
+++ src/sliding_window/char_replacement.rs
@@ -1,11 +1,14 @@
 use std::collections::HashMap;
 
 pub fn character_replacement(s: String, k: i32) -> i32 {
-    let start = 0;
-    let freq_map = HashMap::new();
-    let max_substring = 0;
+    let mut start = 0;
+    let mut freq_map = HashMap::new();
+    let mut max_substring = 0;
     let string: Vec<char> = s.chars().collect();
 
-    //for end in 
+    for end in 0..string.len() {
+        let end_value = string[end];
+        *freq_map.entry(end_value).or_insert(0) += 1;
+    } 
     0
 }
