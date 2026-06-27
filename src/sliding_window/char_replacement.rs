8ca934a700e22d7eebd67ce09e7f09ed24ce97ae
Nathaniel Parah <nathanielparah579@gmail.com>
2026-06-27 23:42:21 +0100

    feat: create pointers, start, and trackers frequency map and pointers
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/sliding_window/char_replacement.rs
+++ src/sliding_window/char_replacement.rs
@@ -1,3 +1,10 @@
+use std::collections::HashMap;
+
 pub fn character_replacement(s: String, k: i32) -> i32 {
+    let start = 0;
+    let freq_map = HashMap::new();
+    let max_substring = 0;
+
+
     0
 }
