ba47043bc1ad7e507478303adace3e6cddb52e30
Nathaniel Parah <nathanielparah579@gmail.com>
2026-06-27 23:47:20 +0100

    feat: remove characters from a  string and store them in an array

--- src/sliding_window/char_replacement.rs
+++ src/sliding_window/char_replacement.rs
@@ -4,7 +4,8 @@ pub fn character_replacement(s: String, k: i32) -> i32 {
     let start = 0;
     let freq_map = HashMap::new();
     let max_substring = 0;
+    let string: Vec<char> = s.chars().collect();
 
-
+    //for end in 
     0
 }
