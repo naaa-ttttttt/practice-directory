eb64e50cf35408c0fbdbf8f1caf0ecd65d5a0c8a
Nathaniel Parah <nathanielparah579@gmail.com>
2026-06-27 15:58:54 +0100

    refactor: change the code style to loop through index and store values in the hashmap
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/sliding_window/variable_window.rs
+++ src/sliding_window/variable_window.rs
@@ -1,13 +1,19 @@
 use std::collections::HashMap;
 
-pub fn longest_substring(words: String) -> i32 {
+pub fn longest_substring(s: String) -> i32 {
     let mut left = 0;
     let mut freq_map = HashMap::new();
     let mut longest_substring = 0;
+    let words: Vec<char> = s.chars().collect();
     
-    for chars in words.chars() {
-        *freq_map.entry(chars).or_insert(0) += 1;
-        longest_substring = freq_map.len();
+    // so we are to loop through the index of the values, 
+    // then get the values at the right pointer, so we can store
+    // it in the hashmap, and so while the count of the values is greater
+    // than one, we should we remove the element at the left pointer,
+    // and if the.
+    for chars in 0..words.len() {
+        let cha = words[chars];
+        *freq_map.entry(&words[chars]).or_insert(0) += 1;
         
         // here we are to check the count of the characters, 
         // and if any characters count is more than one,
@@ -15,7 +21,11 @@ pub fn longest_substring(words: String) -> i32 {
         // check then move the left pointer forward, 
         // rerun the code, i guess.
 
-        if  
+        while 
+        if *freq_map.get(&chars).unwrap() > 1 {
+            words
+            freq_map.remove(&words[left]);
+        }  
         
     }
     0
