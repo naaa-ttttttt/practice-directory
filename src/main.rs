c190afc2eee26639076bbb0a00c63ecd68a51724
Nathaniel Parah <nathanielparah579@gmail.com>
2026-07-02 14:34:50 +0100

    refactor: change executing function
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 //mod two_pointers;
-use crate::sliding_window::char_replacement::character_replacement;
-mod sliding_window;
+use crate::intervals::can_attend_meetings::can_attend_meetings;
+mod intervals;
 
 fn main() {
-    println!("{:?}", character_replacement(String::from("eghghgg"), 2));
+    println!("{:?}", can_attend_meetings(vec![
+        vec![1, 5], 
+        vec![6, 8], 
+        vec![3, 9]]
+        ));
     
     println!("Successful");
 }
