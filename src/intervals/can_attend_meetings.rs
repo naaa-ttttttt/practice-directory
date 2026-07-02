f51c4906cd432ea73cd60810c92fb96bd4a53e75
Nathaniel Parah <nathanielparah579@gmail.com>
2026-07-02 18:49:23 +0100

    docs: write a short algorithm describing the function, refactor: change function return type and make parameter mutable
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/intervals/can_attend_meetings.rs
+++ src/intervals/can_attend_meetings.rs
@@ -1,3 +1,10 @@
-pub fn can_attend_meetings(intervals: Vec<Vec<i32>>) -> i32 {
-    0
+pub fn can_attend_meetings(mut intervals: Vec<Vec<i32>>) -> bool {
+    // check if the array is the list of intervals is empty
+    // if its empty, return true.
+    // we sort the intervals in ascending order, 
+    // we loop through the array,
+    // if the end time in the first array is less than the start time in the 
+    // next array, return false.
+    intervals.sort_by(|a, b| a[0].cmp(&b[0]));
+    true
 }
