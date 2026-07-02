8920611f46155092814fe34d2f642f7fb4b81460
Nathaniel Parah <nathanielparah579@gmail.com>
2026-07-02 18:50:59 +0100

    feat: add a condition to check if intervals list is empty
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/intervals/can_attend_meetings.rs
+++ src/intervals/can_attend_meetings.rs
@@ -5,6 +5,9 @@ pub fn can_attend_meetings(mut intervals: Vec<Vec<i32>>) -> bool {
     // we loop through the array,
     // if the end time in the first array is less than the start time in the 
     // next array, return false.
+    if intervals.is_empty() {
+        return true;
+    }
     intervals.sort_by(|a, b| a[0].cmp(&b[0]));
     true
 }
