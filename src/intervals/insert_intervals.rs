4ac696e0c7f52d7b45f893966fae709829daa762
Nathaniel Parah <nathanielparah579@gmail.com>
2026-07-03 22:21:29 +0100

    feat: create loop checking if start time of new intervals is less than end time of current interval
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/intervals/insert_intervals.rs
+++ src/intervals/insert_intervals.rs
@@ -1,5 +1,14 @@
-pub fn insert(intervals: Vec<Vec<i32>>, new_intervals: Vec<i32>) -> Vec<Vec<i32>> {
-    let meetings: Vec<Vec<i32>> = Vec::new();
+pub fn insert(mut intervals: Vec<Vec<i32>>, new_intervals: Vec<i32>) -> Vec<Vec<i32>> {
+    let mut  meetings: Vec<Vec<i32>> = Vec::new();
+
+    intervals.sort_by(|a, b| a[0].cmp(&b[0]));
+
+    for i in 0..intervals.len() {
+        if new_intervals[0] < intervals[i][0] {
+            meetings.push(intervals[i].clone());
+            continue;
+        } //else if new_intervals[0]{}
+    }
 
     meetings
 }
