97aaec227e9ec13aaa017b65f92d277766aa038c
Nathaniel Parah <nathanielparah579@gmail.com>
2026-07-07 12:18:00 +0100

    feat: create first conditional loop to check for overlaps and push values where they meet
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/intervals/insert_intervals.rs
+++ src/intervals/insert_intervals.rs
@@ -1,15 +1,12 @@
 pub fn insert(mut intervals: Vec<Vec<i32>>, new_intervals: Vec<i32>) -> Vec<Vec<i32>> {
-    let mut  meetings: Vec<Vec<i32>> = Vec::new();
+    let mut  result: Vec<Vec<i32>> = Vec::new();
     let mut i = 0;
     let len = intervals.len();
 
-    intervals.sort_by(|a, b| a[0].cmp(&b[0]));
-
-    // so i check if the start time of the new_intervals is less than the end time of the current interval
-    // and i also check if the end time of the new interval is less than the start time of the next interval
-    // if it satisfies, we push the values to the vector, else, we merge values where they overlap.
-
-
+    while i < len && intervals[i][1] < new_intervals[0] {
+        result.push(intervals[i].clone());
+        i + 1;
+    }
     // so the correction to the algorithm has changed, we create pointers,
     //that is the starting point of our loop through our intervals, we use
     //a conditional loop to check, while our starting interval is our starting 
@@ -20,5 +17,11 @@ pub fn insert(mut intervals: Vec<Vec<i32>>, new_intervals: Vec<i32>) -> Vec<Vec<
     //the end time of the new interval[1], we reassign the first values of the new interval 
     //by finding the minimum value ofthe current interval start times and we reassign the end times by finding the max of the new interval and the current interval.
 
-    meetings
+    result
 }
+
+    // so i check if the start time of the new_intervals is less than the end time of the current interval
+    // and i also check if the end time of the new interval is less than the start time of the next interval
+    // if it satisfies, we push the values to the vector, else, we merge values where they overlap.
+
+
