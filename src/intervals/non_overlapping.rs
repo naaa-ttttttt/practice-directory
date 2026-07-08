8f7de261032344ca7a2abf30b171936246f8be3f
Nathaniel Parah <nathanielparah579@gmail.com>
2026-07-08 14:25:12 +0100

    create a condition to check if array is empty

--- src/intervals/non_overlapping.rs
+++ src/intervals/non_overlapping.rs
@@ -1,10 +1,14 @@
 pub fn non_overlapping_intervals(mut intervals: Vec<Vec<i32>>) -> i32 {
+
+    if intervals.is_empty() {
+        return 0;
+    }
     let mut count = 0;
 
     intervals.sort_by(|a, b| a[1].cmp(&b[1]));
 
     for i in 1..intervals.len() {
-        if intervals[i][0] > intervals[i - 1][1] {
+        if intervals[i][0] < intervals[i - 1][1] {
             count += 1;
         }
     }
