24cf4635cc19e770d811d98067a0294541e90e82
Nathaniel Parah <nathanielparah579@gmail.com>
2026-07-08 13:51:38 +0100

    feat: create loop to run through the array and check for overlaps
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/intervals/non_overlapping.rs
+++ src/intervals/non_overlapping.rs
@@ -1,7 +1,13 @@
 pub fn non_overlapping_intervals(mut intervals: Vec<Vec<i32>>) -> i32 {
-    let count = 0;
+    let mut count = 0;
 
     intervals.sort_by(|a, b| a[1].cmp(&b[1]));
+
+    for i in 1..intervals.len() {
+        if intervals[i][0] > intervals[i - 1][1] {
+            count += 1;
+        }
+    }
 
 
     count
