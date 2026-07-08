01ba3c04991a259f48694c092b28d258fec80d1c
Nathaniel Parah <nathanielparah579@gmail.com>
2026-07-08 13:31:20 +0100

    feat: sort the array in ascending order by there end times, i.e: there second values
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/intervals/non_overlapping.rs
+++ src/intervals/non_overlapping.rs
@@ -1,5 +1,8 @@
-pub fn non_overlapping_intervals(intervals: Vec<Vec<i32>>) -> i32 {
+pub fn non_overlapping_intervals(mut intervals: Vec<Vec<i32>>) -> i32 {
     let count = 0;
+
+    intervals.sort_by(|a, b| a[1].cmp(&b[1]));
+
 
     count
 }
