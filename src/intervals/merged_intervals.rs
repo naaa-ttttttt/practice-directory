3b7d4eba7ca3f38b85ce2ccf97a33687a8505414
Nathaniel Parah <nathanielparah579@gmail.com>
2026-07-09 13:58:47 +0100

    feat: write step to merge the current interval with the previous interval
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/intervals/merged_intervals.rs
+++ src/intervals/merged_intervals.rs
@@ -6,7 +6,8 @@ pub fn merge_intervals(mut intervals: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
 
     for i in 1..intervals.len() {
         if intervals[i][0] >= intervals[i - 1][1] {
-
+            intervals[i][0] = intervals[i][0].min(intervals[i - 1][0]);
+            intervals[i][1] = intervals[i][1].max(intervals[i - 1][1]);
         }
     }
     output
