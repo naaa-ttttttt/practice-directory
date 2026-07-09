67ceb1f671b598148d2760f4cc6d51a1866bfe3d
Nathaniel Parah <nathanielparah579@gmail.com>
2026-07-09 13:42:33 +0100

    feat: create loop and state condition to catch overlaps of intervals

--- src/intervals/merged_intervals.rs
+++ src/intervals/merged_intervals.rs
@@ -3,5 +3,11 @@ pub fn merge_intervals(mut intervals: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
     let output: Vec<Vec<i32>>  = Vec::new();
 
     intervals.sort_by(|a, b| a[0].cmp(&b[0]));
+
+    for i in 1..intervals.len() {
+        if intervals[i][0] >= intervals[i - 1][1] {
+
+        }
+    }
     output
 } 
