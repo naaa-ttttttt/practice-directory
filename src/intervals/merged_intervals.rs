2febaf0a5fdb83e1f334f9de903778f8978669bb
Nathaniel Parah <nathanielparah579@gmail.com>
2026-07-09 13:39:07 +0100

    feat: sort the intervals by there start times in ascending order

--- src/intervals/merged_intervals.rs
+++ src/intervals/merged_intervals.rs
@@ -1,5 +1,7 @@
-pub fn merge_intervals(intervals: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+pub fn merge_intervals(mut intervals: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
 
     let output: Vec<Vec<i32>>  = Vec::new();
+
+    intervals.sort_by(|a, b| a[0].cmp(&b[0]));
     output
 } 
