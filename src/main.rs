d5cd89e8ad2cd8281f61dc34b4687b66e7e8307d
Nathaniel Parah <nathanielparah579@gmail.com>
2026-07-10 18:44:34 +0100

    feat: change function to be executed
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 //mod two_pointers;
-use crate::intervals::merged_intervals::merge_intervals;
+use crate::intervals::employee_free_time::employee_free_time;
 mod intervals;
 
 fn main() {
-    println!("{:?}", merge_intervals(vec![
-        vec![3, 5], 
-        vec![1, 4],
+    println!("{:?}", employee_free_time(vec![
+        vec![
+        vec![3, 5], vec![1, 4]
+        ],
+        vec![
         vec![7, 9],
-        vec![6, 8]
-        ])
+        vec![6, 8],
+        ]])
         );
     
     println!("Successful");
