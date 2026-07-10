0d0b87575967cf28b52a46b07f2754e39e3e1895
Nathaniel Parah <nathanielparah579@gmail.com>
2026-07-10 19:06:44 +0100

    feat: compress the schedule list to hold intervals

--- src/intervals/employee_free_time.rs
+++ src/intervals/employee_free_time.rs
@@ -1,4 +1,11 @@
 pub fn employee_free_time(schedules: Vec<Vec<Vec<i32>>>) -> Vec<Vec<i32>>{
+    let mut flattened_intervals: Vec<Vec<i32>> = Vec::new();
+
+    for sets in schedules {
+        for intervals in sets {
+            flattened_intervals.push(intervals);
+        }
+    }
 
     let mut free_time = Vec::new();
 
