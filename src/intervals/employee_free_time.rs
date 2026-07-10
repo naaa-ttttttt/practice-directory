ee924a2bc23aae50dafcd46c25d8f66376201542
Nathaniel Parah <nathanielparah579@gmail.com>
2026-07-10 19:27:11 +0100

    feat: create condition that will check for non overlaps and push them to the merged array

--- src/intervals/employee_free_time.rs
+++ src/intervals/employee_free_time.rs
@@ -7,7 +7,13 @@ pub fn employee_free_time(schedules: Vec<Vec<Vec<i32>>>) -> Vec<Vec<i32>>{
         }
     }
 
-    let merged = Vec::new();
+    let mut merged: Vec<Vec<i32>> = Vec::new();
+
+    for overlaps in 1..flattened_intervals.len() {
+        if merged.is_empty() || flattened_intervals[overlaps][0] < merged.last().unwrap()[1] {
+            merged.push(flattened_intervals[overlaps].clone());
+        }
+    }
 
     let mut free_time = Vec::new();
 
