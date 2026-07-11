135a36a136b8d2a5fe39b69a404c648cdb1812cb
Nathaniel Parah <nathanielparah579@gmail.com>
2026-07-11 12:32:54 +0100

    feat: import new function to be executed

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,9 @@
 //mod two_pointers;
-use crate::intervals::employee_free_time::employee_free_time;
-mod intervals;
+use crate::stacks::valid_parenthesis::valid_parenthesis;
+mod stacks;
 
 fn main() {
-    println!("{:?}", employee_free_time(vec![
-        vec![
-        vec![3, 5], vec![1, 4]
-        ],
-        vec![
-        vec![7, 9], vec![6, 8],
-        ]
-        ])
-        );
+    println!("{:?}", valid_parenthesis("(){({})}"));
     
     println!("Successful");
 }
