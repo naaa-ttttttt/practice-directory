226564ff929a33c324d3e0877d60de798829efda
Nathaniel Parah <nathanielparah579@gmail.com>
2026-07-11 11:06:35 +0100

    feat: sort the flattened array by start time

--- src/intervals/employee_free_time.rs
+++ src/intervals/employee_free_time.rs
@@ -7,12 +7,16 @@ pub fn employee_free_time(schedules: Vec<Vec<Vec<i32>>>) -> Vec<Vec<i32>>{
         }
     }
 
+    flattened_intervals.sort_by_key(|x| x[0]);
+
     let mut merged: Vec<Vec<i32>> = Vec::new();
 
     for overlaps in 1..flattened_intervals.len() {
         if merged.is_empty() || flattened_intervals[overlaps][0] < merged.last().unwrap()[1] {
             merged.push(flattened_intervals[overlaps].clone());
-        }
+        }// else {
+          //  let last_id = 
+        //}
     }
 
     let mut free_time = Vec::new();
