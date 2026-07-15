99bd2d9c2f4d1b4d482457d3a8ac7e008e005e96
Nathaniel Parah <nathanielparah579@gmail.com>
2026-07-15 16:25:08 +0100

    feat: create stack to store elements, and a hashmap to keep  track of values

--- src/stacks/valid_parenthesis.rs
+++ src/stacks/valid_parenthesis.rs
@@ -1,3 +1,15 @@
+use std::collections::HashMap;
+
 pub fn valid_parenthesis(chars: &str) -> bool {
-    true
+    let mut stack: Vec<char> = Vec::new();
+
+    let mapping = HashMap::from([
+        (')', '('),
+        ('}', '{'),
+        (']', '['),
+    ]);
+
+
+
+    false
 }
