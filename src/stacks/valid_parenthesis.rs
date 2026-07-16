93c96f2bbcadbc270fa6c27e2547f7f49d3439b0
Nathaniel Parah <nathanielparah579@gmail.com>
2026-07-16 10:35:40 +0100

    feat: create loop to go through the characters, checking the values by there keys, if the match, we pop them off the stack else, we push, and check if stack is empty to return true or false

--- src/stacks/valid_parenthesis.rs
+++ src/stacks/valid_parenthesis.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-pub fn valid_parenthesis(chars: &str) -> bool {
+pub fn valid_parenthesis(s: &str) -> bool {
     let mut stack: Vec<char> = Vec::new();
 
     let mapping = HashMap::from([
@@ -9,7 +9,15 @@ pub fn valid_parenthesis(chars: &str) -> bool {
         (']', '['),
     ]);
 
-
-
-    false
+    for ch in s.chars() {
+        if let Some(&open_char) = mapping.get(&ch) {
+            if stack.is_empty() || stack[stack.len() - 1] != open_char {
+                return false;
+            }
+            stack.pop();
+        } else {
+            stack.push(ch);
+        } 
+    }
+    stack.is_empty()
 }
