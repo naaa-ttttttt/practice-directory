a6d5cd77bb28f1c043d9a73851339c7a2cc364f4
Nathaniel Parah <nathanielparah579@gmail.com>
2026-07-19 15:11:04 +0100

    feat: create decode strings module with function signature and return type

---
+++ src/stacks/decode_string.rs
@@ -0,0 +1,8 @@
+pub fn decode_strings(s: String) -> String {
+    let string_stack: Vec<String> = Vec::new();
+    let number_stack: Vec<i32> = Vec::new();
+    let current_string = String::new();
+    let current_number = 0;
+
+    current_string
+}
