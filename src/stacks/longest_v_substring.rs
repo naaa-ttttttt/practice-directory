1ea3f61b49b3fc9b11491ea7865a0e9c6f716d4c
Nathaniel Parah <nathanielparah579@gmail.com>
2026-07-20 12:53:07 +0100

    feat: create condition to push opening parenthesis to the stack

--- src/stacks/longest_v_substring.rs
+++ src/stacks/longest_v_substring.rs
@@ -1,6 +1,12 @@
 pub fn longest_v_substring(s: String) -> i32 {
-    let stack: Vec<char> = Vec::new();
+    let mut stack: Vec<char> = Vec::new();
     let count = 0;
+
+    for ch in s.chars() {
+        if ch == '(' {
+            stack.push(ch);
+        }
+    }
 
     count
 }
