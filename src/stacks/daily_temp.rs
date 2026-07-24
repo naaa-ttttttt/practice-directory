09b2065d89d82b62b797c511678b74c27e4e8e8e
Nathaniel Parah <nathanielparah579@gmail.com>
2026-07-24 22:44:06 +0100

    feat: write algorithm to find wait time of warmer temperatures

--- src/stacks/daily_temp.rs
+++ src/stacks/daily_temp.rs
@@ -1,7 +1,17 @@
 pub fn daily_temp(temp: Vec<i32>) -> Vec<i32> {
     let len = temp.len();
-    let stack: Vec<usize> = Vec::new();
-    let result = vec![0; len];
+    let mut stack: Vec<usize> = Vec::new();
+    let mut result = vec![0; len];
+
+    for i in 0..len {
+        while !stack.is_empty() && temp[i] > temp[*stack.last().unwrap()] {
+            let idx = stack.pop().unwrap();
+            result[idx] = (i - idx) as i32;
+        } 
+        stack.push(i);
+    }
+        
+
 
     result
 }
