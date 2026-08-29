af17262fb0fcf4ff0397521b0e6ea556d884bc3d
Nathaniel Parah <nathanielparah579@gmail.com>
2026-08-29 09:30:48 +0100

    feat: create heap to keep incoming values and there distances

--- src/heap/k_closest.rs
+++ src/heap/k_closest.rs
@@ -1,4 +1,6 @@
+use std::collections::BinaryHeap;
 pub fn k_closest(x: i32, k: i32, values: Vec<i32>) -> Vec<i32> {
+    let heap: BinaryHeap<(i32, i32)> = BinaryHeap::new();
 
     let results: Vec<i32> = Vec::new();
 
