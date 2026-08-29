59c02bbe29567b1fcae539d9aee9588a7b6749c0
Nathaniel Parah <nathanielparah579@gmail.com>
2026-08-29 10:03:06 +0100

    fix: inserted the heap check condition inside the forl loop, a check to see if the heap is equal to k

--- src/heap/k_closest.rs
+++ src/heap/k_closest.rs
@@ -5,10 +5,11 @@ pub fn k_closest(x: i32, k: i32, values: Vec<i32>) -> Vec<i32> {
     for num in values {
         let distance = num.abs_diff(x);
         heap.push((distance.try_into().unwrap(), num));
-    }
 
-    if heap.len() > k.try_into().unwrap() {
-        heap.pop();
+        if heap.len() > k.try_into().unwrap() {
+            heap.pop();
+        }
+
     }
 
     let mut results: Vec<i32> = Vec::new();
