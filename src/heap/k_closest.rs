cd14c2c4c6eb0aef8ecfb71859f35745188641b6
Nathaniel Parah <nathanielparah579@gmail.com>
2026-08-29 09:39:01 +0100

    feat: create for loop and find absolute distance/difference between nums and target: x

--- src/heap/k_closest.rs
+++ src/heap/k_closest.rs
@@ -1,6 +1,11 @@
 use std::collections::BinaryHeap;
 pub fn k_closest(x: i32, k: i32, values: Vec<i32>) -> Vec<i32> {
-    let heap: BinaryHeap<(i32, i32)> = BinaryHeap::new();
+    let mut heap: BinaryHeap<(i32, i32)> = BinaryHeap::new();
+
+    for num in values {
+        let distance = num.abs_diff(x);
+        heap.push((distance.try_into().unwrap(), num));
+    }
 
     let results: Vec<i32> = Vec::new();
 
