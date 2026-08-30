d89781f80e20c81ec773bc053608df79fa412cc4
Nathaniel Parah <nathanielparah579@gmail.com>
2026-08-30 19:33:59 +0100

    feat: call the max depth function and pass its arguments

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,44 @@
 //mod two_pointers;
-use crate::heap::k_closest::k_closest;
-mod heap;
+
+use crate::dfs::max_depth::TreeNode;
+use std::{
+    rc::Rc,
+    cell::RefCell,
+};
+mod dfs;
 
 fn main() {
-    println!("{:?}", k_closest(1, 3, vec![-1, 0, 1, 4, 6]));
+    let results = TreeNode::max_depth(
+        Some(Rc::new(RefCell::new(TreeNode {
+            val: 4,
+            left: Some(Rc::new(RefCell::new(TreeNode {
+                val: 2,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 1,
+                    left: None,
+                    right: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 8,
+                        left: None,
+                        right: None,
+                    }))),
+                }))),
+                right: None,
+            }))),
+            right: Some(Rc::new(RefCell::new(TreeNode {
+                val: 7,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 6,
+                    left: None,
+                    right: None,
+                }))),
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 9,
+                    left: None,
+                    right: None,
+                }))),
+            }))),
+        }))));
+    println!("{:?}", results);
     
     println!("Successful");
 }
