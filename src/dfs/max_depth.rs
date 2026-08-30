b6afffcfff5df0de81e2d23b482f3413c0621d1e
Nathaniel Parah <nathanielparah579@gmail.com>
2026-08-30 20:10:35 +0100

    feat: find the max depth of the three node

--- src/dfs/max_depth.rs
+++ src/dfs/max_depth.rs
@@ -1,6 +1,7 @@
 use std::{
     rc::Rc,
     cell::RefCell,
+    cmp::max,
 };
 #[derive(PartialEq)]
 pub struct TreeNode {
@@ -12,12 +13,16 @@ pub struct TreeNode {
 impl TreeNode {
     pub fn max_depth(node: Option<Rc<RefCell<TreeNode>>>) -> i32 {
         // we create the base case upon which recursion should end.
-        if node == None {
-            return 0;
+
+        if let Some(n) = node {
+            let borrowed = n.borrow();
+            let left_depth = Self::max_depth(borrowed.left.clone());
+            let right_depth = Self::max_depth(borrowed.right.clone());
+
+            1 + max(left_depth, right_depth)
+        } else {
+            0
         }
 
-
-        
-        0
     }
 }
